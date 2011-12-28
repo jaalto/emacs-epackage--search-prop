314224c76c64ae1ef0de926c38a7287dd350c720
Jari Aalto <jari.aalto@cante.net>
2011-12-28 10:06:01 +0200

    epackage/: new
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

---
+++ epackage/search-prop-epkg-autoloads.el
@@ -0,0 +1,4 @@
+(autoload 'search-property                      "search-prop" "" nil)
+(autoload 'search-property-forward              "search-prop" "" t)
+(autoload 'search-property-backward             "search-prop" "" t)
+(provide 'search-prop-epkg-autoloads)
---
+++ epackage/search-prop-epkg-install.el
@@ -0,0 +1,3 @@
+(autoload 'search-property-forward              "search-prop" "" t)
+(autoload 'search-property-backward             "search-prop" "" t)
+(provide 'search-prop-epkg-install)
